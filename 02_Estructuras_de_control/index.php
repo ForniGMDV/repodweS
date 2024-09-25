76480d888212723104e1312940e4928ee6df4aec
Alejo <alejoforni85@gmail.com>
2024-09-25 11:18:46 +0200

    Ejercicios clase dia 25-09

---
+++ 02_Estructuras_de_control/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de contenidos</title>
+    <style>
+        table{
+            border-collapse: collapse;
+            width: 700px;
+        }
+        table th, table td{
+            text-align: center;
+            border: 3px solid deeppink;
+        }
+        a{
+            color: blue;
+            text-decoration: none;
+        }
+        a:hover{
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <caption>Tabla de contenidos</caption>
+        <thead>
+            <tr>
+                <th>Archivo</th>
+                <th>Descripción</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <a href="numeros.php">numeros.php</a>
+                </td>
+                <td>Ejercicios con números y estructuras IF</td>
+            </tr>
+            <td>
+                    <a href="fechas.php">fechas.php</a>
+                </td>
+                <td>Ejercicios de fechas con la función date()</td>
+        </tbody>
+    </table>
+</body>
+</html>
